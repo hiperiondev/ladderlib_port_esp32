ad52362175d944a62cb93011ea17bc0f0b74144a
hiperiondev <egonzalez.hiperion@gmail.com>
2025-02-25 12:12:44 -0300

    Reorder

---
+++ components/cmd_ladderlib/cmd_ladderlib.h
@@ -0,0 +1,19 @@
+/*
+ * cmd_ladderlib.h
+ *
+ *  Created on: 25 feb 2025
+ *      Author: egonzalez
+ */
+
+#ifndef CMD_LADDERLIB_H_
+#define CMD_LADDERLIB_H_
+
+void register_ladder_status(void);
+void register_fs_ls(void);
+void register_fs_rm(void);
+void register_ladder_dump(void);
+void register_ladder_load(void);
+void register_ladder_start(void);
+void register_ladder_stop(void);
+
+#endif /* CMD_LADDERLIB_H_ */
