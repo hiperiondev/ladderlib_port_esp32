3c6842294e40f9c20222f6025953b0f2589ec17d
hiperiondev <egonzalez.hiperion@gmail.com>
2025-03-03 20:04:48 -0300

    Minimal change

--- main/app_main.c
+++ main/app_main.c
@@ -1,11 +1,30 @@
-/* Blink Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+/*
+ * Copyright 2025 Emiliano Gonzalez (egonzalez . hiperion @ gmail . com))
+ * * Project Site: https://github.com/hiperiondev/ladderlib *
+ *
+ * This is based on other projects:
+ *    PLsi (https://github.com/ElPercha/PLsi)
+ *    ccronexpr (https://github.com/staticlibs/ccronexpr)
+ *
+ *    please contact their authors for more information.
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ *
+ */
+ 
 #include <string.h>
 
 #include "cmd_system.h"
@@ -68,6 +87,4 @@ void app_main(void) {
     if (!ladder_ctx_init(&ladder_ctx, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)) {
         ESP_LOGI(TAG, "ERROR first initializing\n");
     }
-
-    vTaskDelay(100 / portTICK_PERIOD_MS);
 }
